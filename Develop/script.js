f067683cb1f2304951c1cb23a1ccf5664cd239b2
Justin B <jbedford94@gmail.com>
2021-07-26 00:22:20 -0400

    change color of rows based on time

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,77 @@
+$(document).ready(function() {
+    var events = [];
+
 
 //Current date and time in header
 var todaysDate = moment().format('dddd, MMM Do YYYY ');
 $("#currentDay").html(todaysDate);
 
-  // apply new class if task is near/over due date
   
-  
-  const rows = document.getElementsByClassName("row");
-  let currentHour = parseInt(moment().format('H'));
-  
-  Array.from(rows).forEach(row => {
-    let
-      rowIdString = row.id,
-      rowHour;
-    if (rowIdString) {
-      rowHour = parseInt(rowIdString);
+window.onload = function(){
+    colorCode();
     }
-    if (rowHour) {
-      // Compares row id to current hour and sets color accordingly
-      if (currentHour === rowHour) {
-        setColor(row, "red");
-      } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-        setColor(row, "green");
-      } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-        setColor(row, "lightgrey");
-      } else {
-        setColor(row, "white");
+  
+  //change color of rows based on time
+
+  var now = new Date().getHours();
+  function colorCode() {
+    if (now > 9) {
+      $("#9amtask").addClass("past");
+    } else if (now >= 9 && now < 10) {
+      $("#9amtask").addClass("present");
+    } else if (now < 9) {
+      $("#9amtask").addClass("future");
+    }
+    if (now > 10) {
+        $("#10amtask").addClass("past");
+      } else if (now >= 10 && now < 11) {
+        $("#10amtask").addClass("present");
+      } else if (now < 10) {
+        $("#10amtask").addClass("future");
       }
-    }
-  });
-  
-  function setColor(element, color) {
-    element.style.backgroundColor = color;
-  }
+      if (now > 11) {
+        $("#11amtask").addClass("past");
+      } else if (now >= 11 && now < 12) {
+        $("#t11amtask").addClass("present");
+      } else if (now < 11) {
+        $("#11amtask").addClass("future");
+      }
+      if (now > 12) {
+        $("#12pmtask").addClass("past");
+      } else if (now >= 12 && now < 13) {
+        $("#12pmtask").addClass("present");
+      } else if (now < 12) {
+        $("#12pmtask").addClass("future");
+      }
+      if (now > 13) {
+        $("#1pmtask").addClass("past");
+      } else if (now >= 13 && now < 14) {
+        $("#1pmtask").addClass("present");
+      } else if (now < 13) {
+        $("#1pmtask").addClass("future");
+      }
+      if (now > 14) {
+        $("#2pmtask").addClass("past");
+      } else if (now >= 14 && now < 15) {
+        $("#2pmtask").addClass("present");
+      } else if (now < 14) {
+        $("#2pmtask").addClass("future");
+      }
+      if (now > 15) {
+        $("#3pmtask").addClass("past");
+      } else if (now >= 15 && now < 16) {
+        $("#3pmtask").addClass("present");
+      } else if (now < 15) {
+        $("#3pmtask").addClass("future");
+      }
+}
+
+
+//save function
+$(".saveBtn").on("click", function() {
+    var value = $(this).siblings(".task").val;
+    localStorage.setItem("events", JSON.stringify(events));
+    
+});
+});
+   
